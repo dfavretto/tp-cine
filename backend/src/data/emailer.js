06dc5ac3285be8dd1f914927419b81b8fbcf733c
David Favretto <david.favretto@southworks.com>
2019-06-18 20:48:57 -0300

    Continuar probando emails con template

--- backend/src/data/emailer.js
+++ backend/src/data/emailer.js
@@ -1,6 +1,7 @@
 const nodemailer = require('nodemailer');
 const Reserva = require('../models/reserva').Reserva;
 const Email = require('email-templates');
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 /**
  * enviar email
  * @param {Reserva} reserva reserva a enviar por mail
@@ -10,19 +11,30 @@ async function sendEmail(reserva) {
         // if (typeof reserva !== Reserva) {
         //     throw new Error('Se debe usar un objeto de tipo Reserva para enviar por email.');
         // }
-
-        var transporter = nodemailer.createTransport({
-            service: "gmail",
-            host: "smtp.gmail.com",
+        let testAccount = await nodemailer.createTestAccount();
+        let transporter = nodemailer.createTransport({
+            host: "smtp.ethereal.email",
+            port: 587,
+            secure: false, // true for 465, false for other ports
             auth: {
-                user: "no.david.favretto",
-                pass: "David66Favretto"
+                user: testAccount.user, // generated ethereal user
+                pass: testAccount.pass // generated ethereal password
             }
         });
 
+
+        // var transporter = nodemailer.createTransport({
+        //     service: "gmail",
+        //     host: "smtp.gmail.com",
+        //     auth: {
+        //         user: "no.david.favretto",
+        //         pass: "David66Favretto"
+        //     }
+        // });
+
         const email = new Email({
             message: {
-                from: '"Reservas 👻" <reservas@tp-cine.com>'
+                from: '"Reservas 👻" <no.david.favretto@gmail.com>'
             },
             // uncomment below to send emails in development/test env:
             // send: true
@@ -38,7 +50,7 @@ async function sendEmail(reserva) {
             .send({
                 template: '../src/data/emails/plantillas',
                 message: {
-                    to: reserva.email
+                    to: 'ddaviddf@gmail.com'//reserva.email
                 }/*,
                 locals: {
                     name: 'Elon'
