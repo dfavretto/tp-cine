651000afe7ebf8e76e91ad8f07093dff4c859fb9
Jere91 <jeremias.hsn@gmail.com>
2019-06-26 19:38:23 -0300

    se preparó el array de asientos en ambos modelos de sala

--- frontend/tp-cine/src/models/sala.js
+++ frontend/tp-cine/src/models/sala.js
@@ -1,14 +1,6 @@
-abstract class Sala {
+class Sala {
     constructor(numero) {
         this.numero = numero;
-        const CANT_FILAS = 5;
-        const BUTACAS_FILA = 6;
-        this.butacas = new Array (CANT_FILAS);
-        this.butacas.forEach(fila => {
-            fila = new Array (BUTACAS_FILA);
-            fila.forEach(butaca => {
-                butaca = false;
-            });
-        });
+        this.butacas = new Array (8);
     }
 }
